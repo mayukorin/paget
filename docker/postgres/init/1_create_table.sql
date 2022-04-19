b172789bac1e7705b1da8e160425fb507e2244a9
mayukorin <mayukuma@r.sea.sannet.ne.jp>
2022-04-19 13:05:19 +0900

    chore: setup go and postgres through docker

---
+++ docker/postgres/init/1_create_table.sql
@@ -0,0 +1,18 @@
+CREATE TABLE PAGET_USER (
+    id BIGINT PRIMARY KEY,
+    slack_user_id VARCHAR(20) NOT NULL,
+    slack_channel_id VARCHAR(20) NOT NULL
+);
+
+CREATE TABLE KEYWORD (
+    id BIGINT PRIMARY KEY,
+    content VARCHAR(20) NOT NULL
+);
+
+CREATE TABLE USER_KEYWORD (
+    id BIGINT PRIMARY KEY,
+    paget_user_id BIGINT,
+    keyword_id BIGINT,
+    foreign key (paget_user_id) references PAGET_USER(id),
+    foreign key (keyword_id) references KEYWORD(id)
+);
---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/mayukorin/paget
+
+go 1.18
+
+require (
+	github.com/gorilla/websocket v1.4.2 // indirect
+	github.com/joho/godotenv v1.4.0 // indirect
+	github.com/lib/pq v1.10.5 // indirect
+	github.com/slack-go/slack v0.10.2 // indirect
+)
