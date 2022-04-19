ce8eb1d205882f9fa31bab2e4523b7c387c38fc8
mayukorin <mayukuma@r.sea.sannet.ne.jp>
2022-04-19 19:00:12 +0900

    feat: add slash command 'add keyword'

---
+++ cmd/api/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+)
+
+func createUserKeyword(c *gin.Context) {
+	fmt.Println("create")
+	/*
+		s, err := slack.SlashCommandParse(r)
+		if err != nil {
+			fmt.Println("error")
+			w.WriteHeader(http.StatusInternalServerError)
+		}
+		fmt.Println(s.Command)
+	*/
+}
+
+func hello(c *gin.Context) {
+	fmt.Println("hello")
+}
+
+func hello2(c *gin.Context) {
+	fmt.Println("hello")
+}
+
+func main() {
+	// http.HandleFunc("/user-keyword-create", createUserKeyword)
+	router := gin.Default()
+	router.GET("/hello", hello)
+	router.POST("/add", createUserKeyword)
+	router.Run(":1323")
+}
