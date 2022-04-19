021924a8d053054d6e9e3124ad93842e52791e7a
mayukorin <mayukuma@r.sea.sannet.ne.jp>
2022-04-19 19:58:33 +0900

    fix: modify gin to httpHandleFunc to use slack-go

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -2,20 +2,36 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/slack-go/slack"
 )
 
+/*
 func createUserKeyword(c *gin.Context) {
 	fmt.Println("create")
-	/*
-		s, err := slack.SlashCommandParse(r)
-		if err != nil {
-			fmt.Println("error")
-			w.WriteHeader(http.StatusInternalServerError)
-		}
-		fmt.Println(s.Command)
-	*/
+
+	s, err := slack.SlashCommandParse(r)
+	if err != nil {
+		fmt.Println("error")
+		w.WriteHeader(http.StatusInternalServerError)
+	}
+	fmt.Println(s.Command)
+
+}
+*/
+
+func createUserKeyword(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("create")
+
+	s, err := slack.SlashCommandParse(r)
+	if err != nil {
+		fmt.Println("error")
+		w.WriteHeader(http.StatusInternalServerError)
+	}
+	fmt.Println(s.Command)
+
 }
 
 func hello(c *gin.Context) {
@@ -27,10 +43,13 @@ func hello2(c *gin.Context) {
 }
 
 func main() {
-	// http.HandleFunc("/user-keyword-create", createUserKeyword)
+	http.HandleFunc("/user-keyword-create", createUserKeyword)
 	fmt.Println("main")
-	router := gin.Default()
-	router.GET("/hello", hello)
-	router.POST("/add", createUserKeyword)
-	router.Run()
+	http.ListenAndServe(":53867", nil)
+	/*
+		router := gin.Default()
+		router.GET("/hello", hello)
+		router.POST("/add", createUserKeyword)
+		router.Run()
+	*/
 }
