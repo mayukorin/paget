19461b173c4ac80fcb6ddaaf914d91b7509a47ab
mayukorin <mayukuma@r.sea.sannet.ne.jp>
2022-04-20 15:14:24 +0900

    feat: add delete user_keyword

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -26,6 +26,65 @@ func createUserKeyword(c *gin.Context) {
 }
 */
 
+func deleteUserKeyword(w http.ResponseWriter, r *http.Request) {
+
+	db, err := sql.Open("postgres", os.Getenv("DATABASE_URL"))
+	if err != nil {
+		fmt.Printf("error opening database: %q\n", err)
+	}
+
+	fmt.Println("delete")
+
+	s, err := slack.SlashCommandParse(r)
+	if err != nil {
+		fmt.Println("error")
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	var deleteKeywordId int64
+	if err := db.QueryRow("SELECT id FROM keyword WHERE content = $1", s.Text).Scan(&deleteKeywordId); err != nil {
+		if err != sql.ErrNoRows {
+			fmt.Printf("error when select keyword:%q\n", err)
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		fmt.Printf("keyword canot found")
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	var userId int64
+	if err := db.QueryRow("SELECT id FROM slack_user WHERE slack_id = $1", s.UserID).Scan(&userId); err != nil {
+		if err != sql.ErrNoRows {
+			fmt.Printf("error when select slack_user:%q\n", err)
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		fmt.Printf("slack_user canot found")
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	_, err = db.Exec("DELETE FROM user_keyword WHERE keyword_id = $1 and slack_user_id = $2", deleteKeywordId, userId)
+	if err != nil {
+		fmt.Printf("user_keyword cannot delete: %q\n", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	params := &slack.Msg{Text: s.Text + "を検索のキーワードから削除しました！"}
+	b, err := json.Marshal(params)
+	if err != nil {
+		fmt.Println("error")
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	fmt.Println("okk")
+	w.Write(b)
+
+}
+
 func createUserKeyword(w http.ResponseWriter, r *http.Request) {
 
 	db, err := sql.Open("postgres", os.Getenv("DATABASE_URL"))
@@ -65,20 +124,7 @@ func createUserKeyword(w http.ResponseWriter, r *http.Request) {
 			return
 		}
 		fmt.Println("keyword not found")
-		/*
-			r, err := db.Exec("INSERT INTO keyword(content) values($1)", s.Text)
-			if err != nil {
-				fmt.Printf("keyword cannot create:%q\n", err)
-				w.WriteHeader(http.StatusInternalServerError)
-				return
-			}
-			keywordId, err = r.LastInsertId()
-			if err != nil {
-				fmt.Printf("keywordId cannot get:%q\n", err)
-				w.WriteHeader(http.StatusInternalServerError)
-				return
-			}
-		*/
+
 		if err := db.QueryRow("INSERT INTO keyword(content) values($1) RETURNING id", s.Text).Scan(&keywordId); err != nil {
 			fmt.Printf("keyword canot create:%q\n", err)
 			w.WriteHeader(http.StatusInternalServerError)
@@ -121,6 +167,7 @@ func helloWorld(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	http.HandleFunc("/add", createUserKeyword)
+	http.HandleFunc("/delete", deleteUserKeyword)
 	http.HandleFunc("/hello", helloWorld)
 	fmt.Println("main")
 	http.ListenAndServe(":"+os.Getenv("PORT"), nil)
