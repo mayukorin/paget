01b9f331a53150b8676f6a47717b8eef49619da0
mayukorin <mayukuma@r.sea.sannet.ne.jp>
2022-04-20 14:40:57 +0900

    fix: change auto_increment to serial for psql and drop table order for dependent

--- cmd/db/main.go
+++ cmd/db/main.go
@@ -12,20 +12,7 @@ func main() {
 	db, err := sql.Open("postgres", os.Getenv("DATABASE_URL"))
 	if err != nil {
 		fmt.Printf("error opening database: %q", err)
-	}
-
-	const sql4 = `
-	DROP TABLE SLACK_USER;
-	`
-	if _, err := db.Exec(sql4); err != nil {
-		fmt.Printf("slack_user drop 失敗: %q", err)
-	}
-
-	const sql5 = `
-	DROP TABLE KEYWORD;
-	`
-	if _, err := db.Exec(sql5); err != nil {
-		fmt.Printf("keyword drop 失敗: %q", err)
+		return
 	}
 
 	const sql6 = `
@@ -33,32 +20,51 @@ func main() {
 	`
 	if _, err := db.Exec(sql6); err != nil {
 		fmt.Printf("user_keyword drop 失敗: %q", err)
+		return
+	}
+
+	const sql4 = `
+	DROP TABLE SLACK_USER;
+	`
+	if _, err := db.Exec(sql4); err != nil {
+		fmt.Printf("slack_user drop 失敗: %q", err)
+		return
+	}
+
+	const sql5 = `
+	DROP TABLE KEYWORD;
+	`
+	if _, err := db.Exec(sql5); err != nil {
+		fmt.Printf("keyword drop 失敗: %q", err)
+		return
 	}
 
 	const sql1 = `
 	CREATE TABLE SLACK_USER (
-		id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+		id SERIAL NOT NULL,
 		slack_id VARCHAR(20) NOT NULL,
 		slack_channel_id VARCHAR(20) NOT NULL
 	);
 	`
 	if _, err := db.Exec(sql1); err != nil {
 		fmt.Printf("slack_user 失敗: %q", err)
+		return
 	}
 
 	const sql2 = `
 	CREATE TABLE KEYWORD (
-		id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+		id SERIAL NOT NULL,
 		content VARCHAR(20) NOT NULL
 	);
 	`
 	if _, err := db.Exec(sql2); err != nil {
 		fmt.Printf("keyword 失敗: %q", err)
+		return
 	}
 
 	const sql3 = `
 	CREATE TABLE USER_KEYWORD (
-		id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+		id SERIAL NOT NULL,
 		slack_user_id BIGINT,
 		keyword_id BIGINT,
 		foreign key (slack_user_id) references SLACK_USER(id),
@@ -67,5 +73,6 @@ func main() {
 	`
 	if _, err := db.Exec(sql3); err != nil {
 		fmt.Printf("user keyword 失敗: %q", err)
+		return
 	}
 }
