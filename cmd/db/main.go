b0f29e0d2d4066d9ee990557c74172328c7d4179
mayukorin <mayukuma@r.sea.sannet.ne.jp>
2022-04-20 13:22:31 +0900

    feat: add table create

---
+++ cmd/db/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"os"
+)
+
+func main() {
+	db, err := sql.Open("postgres", os.Getenv("DATABASE_URL"))
+	if err != nil {
+		fmt.Printf("error opening database: %q", err)
+	}
+	const sql1 = `
+	CREATE TABLE SLACK_USER (
+		id BIGINT PRIMARY KEY,
+		slack_id VARCHAR(20) NOT NULL,
+		slack_channel_id VARCHAR(20) NOT NULL
+	);
+	`
+	if _, err := db.Exec(sql1); err != nil {
+		fmt.Printf("slack_user 失敗: %q", err)
+	}
+
+	const sql2 = `
+	CREATE TABLE KEYWORD (
+		id BIGINT PRIMARY KEY,
+		content VARCHAR(20) NOT NULL
+	);
+	`
+	if _, err := db.Exec(sql2); err != nil {
+		fmt.Printf("keyword 失敗: %q", err)
+	}
+
+	const sql3 = `
+	CREATE TABLE USER_KEYWORD (
+		id BIGINT PRIMARY KEY,
+		slack_user_id BIGINT,
+		keyword_id BIGINT,
+		foreign key (slack_user_id) references SLACK_USER(id),
+		foreign key (keyword_id) references KEYWORD(id)
+	);	
+	`
+	if _, err := db.Exec(sql3); err != nil {
+		fmt.Printf("user keyword 失敗: %q", err)
+	}
+}
--- docker/postgres/init/1_create_table.sql
+++ docker/postgres/init/1_create_table.sql
@@ -1,6 +1,6 @@
-CREATE TABLE USER (
+CREATE TABLE SLACK_USER (
     id BIGINT PRIMARY KEY,
-    slack_user_id VARCHAR(20) NOT NULL,
+    slack_id VARCHAR(20) NOT NULL,
     slack_channel_id VARCHAR(20) NOT NULL
 );
 
@@ -11,8 +11,8 @@ CREATE TABLE KEYWORD (
 
 CREATE TABLE USER_KEYWORD (
     id BIGINT PRIMARY KEY,
-    paget_user_id BIGINT,
+    slack_user_id BIGINT,
     keyword_id BIGINT,
-    foreign key (paget_user_id) references PAGET_USER(id),
+    foreign key (slack_user_id) references SLACK_USER(id),
     foreign key (keyword_id) references KEYWORD(id)
 );
