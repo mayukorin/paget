7a7f45a1f69d56de86af4e985182cdf2bf336822
mayukorin <mayukuma@r.sea.sannet.ne.jp>
2022-04-27 10:44:35 +0900

    refactor:add query.go which cope with db query for DRY

---
+++ query.go
@@ -0,0 +1,19 @@
+package paget
+
+import (
+	"database/sql"
+	"fmt"
+)
+
+func findUserId(db *sql.DB, slack_user_id string) (userId int64, err error) {
+	userId = 0
+	if err = db.QueryRow("SELECT id FROM slack_user WHERE slack_id = $1", slack_user_id).Scan(&userId); err != nil {
+		if err != sql.ErrNoRows {
+			fmt.Printf("error when select slack_user:%q\n", err)
+			return
+		}
+		fmt.Printf("slack_user canot found")
+		return
+	}
+	return
+}
