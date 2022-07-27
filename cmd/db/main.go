b6b14c65c64ff619dd2385e6d17cab5728f46939
mayukorin <mayukuma@r.sea.sannet.ne.jp>
2022-07-27 12:37:02 +0900

    feat: add column latest_mattched_paper to user table

--- cmd/db/main.go
+++ cmd/db/main.go
@@ -23,56 +23,66 @@ func main() {
 			return
 		}
 	*/
+	/*
+
+		const sql4 = `
+		DROP TABLE SLACK_USER;
+		`
+		if _, err := db.Exec(sql4); err != nil {
+			fmt.Printf("slack_user drop 失敗: %q", err)
+			return
+		}
+
+		const sql5 = `
+		DROP TABLE KEYWORD;
+		`
+		if _, err := db.Exec(sql5); err != nil {
+			fmt.Printf("keyword drop 失敗: %q", err)
+			return
+		}
+
+		const sql1 = `
+		CREATE TABLE SLACK_USER (
+			id SERIAL PRIMARY KEY,
+			slack_id VARCHAR(20) NOT NULL,
+			slack_channel_id VARCHAR(20) NOT NULL
+		);
+		`
+		if _, err := db.Exec(sql1); err != nil {
+			fmt.Printf("slack_user create 失敗: %q", err)
+			return
+		}
+
+		const sql2 = `
+		CREATE TABLE KEYWORD (
+			id SERIAL PRIMARY KEY,
+			content VARCHAR(20) NOT NULL
+		);
+		`
+		if _, err := db.Exec(sql2); err != nil {
+			fmt.Printf("keyword create 失敗: %q", err)
+			return
+		}
+
+		const sql3 = `
+		CREATE TABLE USER_KEYWORD (
+			id SERIAL PRIMARY KEY,
+			slack_user_id BIGINT,
+			keyword_id BIGINT,
+			foreign key (slack_user_id) references SLACK_USER(id),
+			foreign key (keyword_id) references KEYWORD(id)
+		);
+		`
+		if _, err := db.Exec(sql3); err != nil {
+			fmt.Printf("user keyword create 失敗: %q", err)
+			return
+		}
+	*/
 
 	const sql4 = `
-	DROP TABLE SLACK_USER;
+	ALTER TABLE USER ADD COLUMN latest_matched_paper TEXT;
 	`
 	if _, err := db.Exec(sql4); err != nil {
-		fmt.Printf("slack_user drop 失敗: %q", err)
-		return
-	}
-
-	const sql5 = `
-	DROP TABLE KEYWORD;
-	`
-	if _, err := db.Exec(sql5); err != nil {
-		fmt.Printf("keyword drop 失敗: %q", err)
-		return
-	}
-
-	const sql1 = `
-	CREATE TABLE SLACK_USER (
-		id SERIAL PRIMARY KEY,
-		slack_id VARCHAR(20) NOT NULL,
-		slack_channel_id VARCHAR(20) NOT NULL
-	);
-	`
-	if _, err := db.Exec(sql1); err != nil {
-		fmt.Printf("slack_user create 失敗: %q", err)
-		return
-	}
-
-	const sql2 = `
-	CREATE TABLE KEYWORD (
-		id SERIAL PRIMARY KEY,
-		content VARCHAR(20) NOT NULL
-	);
-	`
-	if _, err := db.Exec(sql2); err != nil {
-		fmt.Printf("keyword create 失敗: %q", err)
-		return
-	}
-
-	const sql3 = `
-	CREATE TABLE USER_KEYWORD (
-		id SERIAL PRIMARY KEY,
-		slack_user_id BIGINT,
-		keyword_id BIGINT,
-		foreign key (slack_user_id) references SLACK_USER(id),
-		foreign key (keyword_id) references KEYWORD(id)
-	);	
-	`
-	if _, err := db.Exec(sql3); err != nil {
 		fmt.Printf("user keyword create 失敗: %q", err)
 		return
 	}
